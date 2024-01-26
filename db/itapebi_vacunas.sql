50a4070f55b4075df76d22d842f988278c961177
luisgb23 <luisgeymonatbidart@gmail.com>
2024-01-26 18:09:19 -0300

    dbvacunas

---
+++ db/itapebi_vacunas.sql
@@ -0,0 +1,59 @@
+CREATE DATABASE  IF NOT EXISTS `itapebi` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `itapebi`;
+-- MySQL dump 10.13  Distrib 8.0.36, for macos14 (arm64)
+--
+-- Host: localhost    Database: itapebi
+-- ------------------------------------------------------
+-- Server version	8.1.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `vacunas`
+--
+
+DROP TABLE IF EXISTS `vacunas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `vacunas` (
+  `VacunaId` int NOT NULL AUTO_INCREMENT,
+  `VacunaFchIngreso` date DEFAULT NULL,
+  `MascotaId` int DEFAULT NULL,
+  `VacunaFchVenc` date DEFAULT NULL,
+  `VacunaFchModificacion` datetime DEFAULT NULL,
+  `VacunaEstado` smallint DEFAULT NULL,
+  PRIMARY KEY (`VacunaId`),
+  KEY `vacunas_mascotas_FK` (`MascotaId`),
+  CONSTRAINT `vacunas_mascotas_FK` FOREIGN KEY (`MascotaId`) REFERENCES `mascotas` (`MascotaId`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vacunas`
+--
+
+LOCK TABLES `vacunas` WRITE;
+/*!40000 ALTER TABLE `vacunas` DISABLE KEYS */;
+INSERT INTO `vacunas` VALUES (1,'2024-01-26',26,'2024-01-25','2024-01-26 17:15:54',1),(2,'2024-01-26',27,'2024-02-01',NULL,1),(3,'2024-01-26',28,'2024-07-01',NULL,0);
+/*!40000 ALTER TABLE `vacunas` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-01-26 17:30:58
